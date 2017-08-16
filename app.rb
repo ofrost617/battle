1039c37e18b4177c9e3c275811fe279029318305
Olivia Frost <olivia.frost617@googlemail.com>
2017-08-16 16:17:32 +0100

    post redirect get pattern mid way

--- app.rb
+++ app.rb
@@ -1,16 +1,28 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+
+enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-  	@player1 = params[:player1]
-  	@player2 = params[:player2]
-  	erb(:play)
+    session[@player1]
+    session[@player2]
+    redirect '/play'
+  	# @player1 = params[:player1]
+  	# @player2 = params[:player2]
   end 
+
+  get '/play' do
+    @player1
+    @player2
+    erb(:play)
+  end
+  
 
   run! if app_file == $0
 end
